a6678dcc949b2c01f6a6d2c8338818f0e82b3025
megit44 <u.megumi.0724@gmail.com>
2024-09-11 15:16:11 +0900

    新規登録機能の実装
    新規登録画面のビューファイルの編集、バリデーション、エラーメッセージの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: { message: 'を入力してください' }
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英字と数字の両方を含めて設定してください' }
+
+  validates :last_name, :first_name, presence: { message: 'を入力してください' },
+                                     format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+
+  validates :last_name_reading, :first_name_reading, presence: { message: 'を入力してください' },
+                                                     format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
+
+  validates :birthday, presence: { message: 'を選択してください' }
 end
