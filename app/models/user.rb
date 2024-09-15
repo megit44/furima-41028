fda6813841c42ffd890d84edef79cc3ff6868a3d
megit44 <u.megumi.0724@gmail.com>
2024-09-15 22:04:52 +0900

    Userモデルの単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: { message: 'を入力してください' }
+  validates :nickname, presence: { message: "can't be blank" }
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英字と数字の両方を含めて設定してください' }
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
 
-  validates :last_name, :first_name, presence: { message: 'を入力してください' },
-                                     format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :last_name, :first_name, presence: { message: "can't be blank" },
+                                     format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
 
-  validates :last_name_reading, :first_name_reading, presence: { message: 'を入力してください' },
-                                                     format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
+  validates :last_name_reading, :first_name_reading, presence: { message: "can't be blank" },
+                                                     format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
 
-  validates :birthday, presence: { message: 'を選択してください' }
+  validates :birthday, presence: { message: "can't be blank" }
 end
