1658a2552e9f917c467500b90d01076e72e7fede
megit44 <u.megumi.0724@gmail.com>
2024-09-19 17:23:08 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,14 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
   has_one :order
   has_one_attached :image
+
+  validates :image, :name, :description, :price, presence: { message: "can't be blank" }
+
+  validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id,
+            numericality: { other_than: 0, message: "can't be blank" }
+
+  validates :price, presence: { message: "can't be blank" },
+                    numericality: { only_integer: true, message: 'is not a number' },
+                    numericality: { greater_than_or_equal_to: 300, message: 'must be greater than or equal to 300' },
+                    numericality: { less_than_or_equal_to: 9_999_999, message: 'must be less than or equal to 9999999' }
 end
