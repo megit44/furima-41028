94401f4eb99098930270bcac85f58d51d8ef8002
megit44 <u.megumi.0724@gmail.com>
2024-09-24 13:59:17 +0900

    Itemモデルの単体テストの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name               { Faker::Commerce.product_name }
+    description        { Faker::Lorem.sentence }
+    category_id        { 1 }
+    condition_id       { 1 }
+    shipping_fee_id    { 1 }
+    prefecture_id      { 1 }
+    shipping_day_id    { 1 }
+    price              { 500 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
