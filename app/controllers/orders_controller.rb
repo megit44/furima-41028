1340793c8ed390d83c51965e7a8d3be1c0b812b3
megit44 <u.megumi.0724@gmail.com>
2024-10-22 17:16:25 +0900

    モデル、コントローラーの作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_address_params)
+    @order_address.user_id = current_user.id
+    @order_address.item_id = @item.id
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
+  end
+end
