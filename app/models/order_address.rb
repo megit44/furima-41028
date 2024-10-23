a56d97c308e35f15aa097cca16f6cca5091d062a
megit44 <u.megumi.0724@gmail.com>
2024-10-23 22:19:48 +0900

    トークンに対するバリデーション設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,6 +4,7 @@ class OrderAddress
                 :token
 
   with_options presence: true do
+    validates :token, presence: true
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
